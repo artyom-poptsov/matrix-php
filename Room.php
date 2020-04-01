b5eebb36789791acc6fbff78282a3f88e579e575
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-04-01 08:32:45 +0300

    Room.php: Add Doxygen comments

--- Room.php
+++ Room.php
@@ -2,18 +2,38 @@
 
 namespace matrix;
 
+/**
+ * This class describes a Matrix room.
+ */
 class Room {
     private $alias;
     private $id;
 
+    /**
+     * The main constructor.
+     *
+     * @param $alias Room alias.
+     * @param $id    Room ID.
+     */
     public function __construct($alias, $id) {
         $this->alias = $alias;
         $this->id    = $id;
     }
 
+    /**
+     * Get room alias.
+     *
+     * @return The room alias.
+     */
     public function get_alias() {
         return $this->alias;
     }
+
+    /**
+     * Get room ID.
+     *
+     * @return The room ID.
+     */
     public function get_id() {
         return $this->id;
     }
