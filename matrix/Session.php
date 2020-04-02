df54af5d30e3de20fb5508437dcd77d9d4c2cf88
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-04-02 19:13:51 +0300

    Session (get_user_info, is_username_available): New methods
    * matrix/Session.php (get_user_info, is_username_available): New public methods.
    * matrix/common.php: Add Synapse-specific constants.

--- matrix/Session.php
+++ matrix/Session.php
@@ -118,6 +118,56 @@ class Session {
     }
 
     /**
+     * ADMIN ONLY: Check if a username is available for registration.
+     *
+     * XXX: This only works with Synapse Matrix server.
+     *
+     * @param $username Username to check.
+     * @return true if username is available, false otherwise.
+     * @throws Matrix_exception on errors.
+     */
+    public function is_username_available($username) {
+        $json = $this->get_user_info($username);
+        return $json == NULL;
+    }
+
+    /**
+     * ADMIN ONLY: Get information about a specific user.
+     *
+     * XXX: This only works with Synapse Matrix server.
+     *
+     * @param $username Username to use.
+     * @return JSON with user information; NULL if user does not exist.
+     * @throws Matrix_exception on errors.
+     */
+    public function get_user_info($username) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_VERBOSE, true);
+        curl_setopt($curl, CURLOPT_URL, $this->server_location
+                    . SYNAPSE_URL . 'admin/' . SYNAPSE_API_VERSION
+                    . '/users/' . $username
+                    . "?access_token=" . $this->access_token);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, 0);
+
+        $result = curl_exec($curl);
+        curl_close($curl);
+        if ($result) {
+            $json = json_decode($result, true);
+            var_dump($json);
+            if (array_key_exists('errcode', $json)) {
+                if ($json['errcode'] == 'M_NOT_FOUND') {
+                    return NULL;
+                }
+                throw new Matrix_exception($json['errcode'], $json['error']);
+            }
+            return $json;
+        } else {
+            throw new Matrix_exception($json['errcode'], $json['error']);
+        }
+    }
+
+    /**
      * Change user password.
      *
      * XXX: This only works when user's old password is needed for the 2nd stage
--- matrix/common.php
+++ matrix/common.php
@@ -2,7 +2,12 @@
 
 //// Constants.
 
+// Matrix constants.
 define("MATRIX_REGISTER_URL", "/_matrix/client/r0/admin/register");
 define("MATRIX_CLIENT_URL",   "/_matrix/client/r0");
+
+// Synapse-specific constants.
+define("SYNAPSE_URL", "/_synapse/");
+define("SYNAPSE_API_VERSION", "v2");
 
 ?>
