d6180839f422eaa8eb2a3da7508f08bf327acd9d
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-04-03 13:04:22 +0300

    matrix/common.php (is_fqn, make_fqn): New procedures

--- matrix/common.php
+++ matrix/common.php
@@ -10,4 +10,28 @@ define("MATRIX_CLIENT_URL",   "/_matrix/client/r0");
 define("SYNAPSE_URL", "/_synapse/");
 define("SYNAPSE_API_VERSION", "v2");
 
+/**
+ * Check if a $name is a fully qualified name (FQN.)  E.g.
+ *     '@avp:example.ru'
+ *
+ * @param $name Name to check.
+ * @return true if $name is a fully qualified user name, false otherwise.
+ */
+function is_fqn($name) {
+    return preg_match('/@.*:.*/', $user_id) > 0;
+}
+
+/**
+ * Make Matrix fully qualified name (FQN.)
+ *
+ * @param $name Name to use.
+ * @param $server Server name.
+ * @return A fully qualified name string.
+ */
+function make_fqn($name, $server) {
+    return '@' . $username . ':' . $server;
+}
+
+
+
 ?>
