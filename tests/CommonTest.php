24757903f5ddbe58d2941e509db56146872c01e0
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-04-03 19:29:02 +0300

    tests/CommonTest.php: New test suite

---
+++ tests/CommonTest.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+include "matrix/common.php";
+
+use \matrix\Room;
+
+use PHPUnit\Framework\TestCase;
+
+final class CommonTest extends TestCase {
+    public function test_FQN_predicate_works(): void {
+        $this->assertEquals(is_fqn("test"), false);
+        $this->assertEquals(is_fqn("@alice:example.com"), true);
+    }
+}
