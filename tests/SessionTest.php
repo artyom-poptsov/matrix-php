f3224420b480c67cd3f5b365a625d77b880778d7
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-04-03 19:57:39 +0300

    tests/SessionTest.php (test_create_room): New TC

--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -1,7 +1,8 @@
 <?php declare(strict_types=1);
 
-include "matrix/Session.php";
-include "matrix/Matrix_client.php";
+include_once "matrix/Session.php";
+include_once "matrix/Matrix_client.php";
+include_once "matrix/common.php";
 
 use PHPUnit\Framework\TestCase;
 use \matrix\Session;
@@ -18,5 +19,22 @@ final class SessionTest extends TestCase {
         $this->assertEquals($session->get_user_id(),         $user_id);
         $this->assertEquals($session->get_access_token(),    $access_token);
         $this->assertEquals($session->get_matrix_client(),   $matrix_client);
+    }
+
+    public function test_create_room(): void {
+        $server_location = 'https://example.org/';
+        $user_id         = '@alice:example.org/';
+        $access_token    = 'secret-token';
+        $room_alias      = "test-room";
+        $matrix_client = $this->createMock(Matrix_client::class);
+
+        $matrix_client->expects($this->once())
+                      ->method('post')
+                      ->with(MATRIX_CLIENT_URL . '/createRoom',
+                             [ 'room_alias_name' => $room_alias ],
+                             [ 'access_token'    => $access_token ] );
+
+        $session = new Session($matrix_client, $user_id, $access_token);
+        $room    = $session->create_room($room_alias);
     }
 }
