f73e9efe45b4bb130a33eb05fdf5213f2b890ef0
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-04-03 19:43:19 +0300

    tests/HTTPClientTest.php: Add to the repository

---
+++ tests/HTTPClientTest.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+include "matrix/HTTP_client.php";
+
+use \matrix\HTTP_client;
+
+use PHPUnit\Framework\TestCase;
+
+final class HTTPClientTest extends TestCase {
+    public function test_HTTP_client_creation(): void {
+        $server_location = 'https://example.org:8008/';
+        $client = new HTTP_client($server_location);
+        $this->assertEquals($client->get_server(), $server_location);
+        $this->assertEquals($client->get_domain(), 'example.org');
+        $this->assertEquals($client->get_port(),   '8008');
+    }
+};
+
+?>
