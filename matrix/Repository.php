285965d799c3b925c9d88fceee2e207c6f006791
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-07-21 19:58:36 +0300

    Repository (upload): Return a 'Content_URI' instance
    * matrix/Repository.php (Repository::upload): Return a 'Content_URI' instance.

--- matrix/Repository.php
+++ matrix/Repository.php
@@ -6,6 +6,7 @@ namespace matrix;
 
 require_once(dirname(__FILE__) . "/common.php");
 require_once(dirname(__FILE__) . "/Admin_session.php");
+require_once(dirname(__FILE__) . "/Content_URI.php");
 
 class Repository {
     protected Session $session;
@@ -16,8 +17,15 @@ class Repository {
         $this->session = $session;
     }
 
+    /**
+     * Upload a file to the content repository.
+     *
+     * @param string $file_path A path to the local file.
+     * @param string $content_type A type of the file contents.
+     * @return Content_URI A content URI of the uploaded file.
+     */
     public function upload(string $file_path,
-                           string $content_type)  {
+                           string $content_type) : Content_URI {
         $matrix_client = $this->session->get_matrix_client();
         $access_token  = $this->session->get_access_token();
 
@@ -27,6 +35,6 @@ class Repository {
             [ 'access_token' => $access_token ],
             [ 'Content-Type' => $content_type ]
         );
-        return $json;
+        return new Content_URI($json['content_uri']);
     }
 }
